7a7a6a6dd7111595ae0b93d3d452d0329df84aee
Shi Woo, Park <custardcream@kakao.com>
2021-08-29 15:33:40 +0900

    v0.0.3

--- UPbit Tracker/UPbit-tracker.py
+++ UPbit Tracker/UPbit-tracker.py
@@ -1,26 +1,37 @@
 # encoding: utf-8
 
 import sys
-import time
 from workflow import Workflow, ICON_ERROR, web
+upbit = dict()
 
 def format_strings_from_quote(ticker, data):
 	price = '{:,.0f}'.format(data[0]['trade_price'])
 	high = '{:,.0f}'.format(data[0]['high_price'])
 	low = '{:,.0f}'.format(data[0]['low_price'])
-	change = '{:,.2f}'.format(data[0]['change_rate']*100)
+	change = '{:.2f}'.format(data[0]['change_rate']*100)
 	change_price = '{:,.0f}'.format(data[0]['change_price'])
+	sign = '+' if float(change) >= 0 else ''
 	
 	formatted = {}
-	formatted['title'] = '{}: {} KRW ( {}%, {} KRW )'.format(ticker, price, change, change_price)
-	formatted['subtitle'] = '24hr high: {} KRW | 24hr low: {} KRW'.format(high, low)
+	formatted['title'] = u'{} ({}): {}원 ( {}{}%, {}{}원 )'.format(upbit[ticker], ticker, price, sign, change, sign, change_price)
+	formatted['subtitle'] = u'일봉 고가: {}원 | 일봉 저가: {}원'.format(high, low)
 	return formatted
 
 def get_time(data):
-	time = data[0]['candle_date_time_kst']
-	wf.add_item(title="Info Time : " + time, icon='clock.png')
+	time = data[0]['candle_date_time_kst'].split('T')
+	wf.add_item(title=u"가격 기준 시각 : " + time[0] + ', ' + time[1], icon='clock.png')
 
 def main(wf):
+	# Get coin name traded in KRW
+	url = "https://api.upbit.com/v1/market/all"
+	r = web.get(url)
+	
+	r.raise_for_status()
+	result = r.json()
+	for upbitCoin in result:
+		if upbitCoin['market'][0:3] == 'KRW':
+			upbit[upbitCoin['market'][4:]] = upbitCoin['korean_name']
+
 	# Get query from Alfred
 	if len(wf.args):
 		query = wf.args[0]
@@ -44,20 +55,20 @@ def main(wf):
 					subtitle=formatted['subtitle'],
 					arg='https://upbit.com/exchange?code=CRIX.UPBIT.KRW-' + ticker,
 					valid=True,
-					icon= 'icon/{}.png'.format(ticker.lower())) # ICON_WEB
+					icon= 'icon/{}.png'.format(ticker.lower()))
 
 			except:
-				wf.add_item(title='Couldn\'t find a quote for that symbol.',
-					subtitle='Please try again.',
+				wf.add_item(title=u'입력하신 티커와 일치하는 가상화폐를 찾지 못했습니다.',
+					subtitle=u'티커는 끝까지 입력해주세요.',
 					icon=ICON_ERROR)
 		except:
-			wf.add_item(title='Couldn\'t find a quote for that symbol.',
-				subtitle='Please try again.',
+			wf.add_item(title=u'입력하신 티커와 일치하는 가상화폐를 찾지 못했습니다.',
+				subtitle=u'티커는 끝까지 입력해주세요.',
 				icon=ICON_ERROR)
 
 	else:
-		coinlist = ['KRW-BTC','KRW-ETH','KRW-ADA','KRW-XRP','KRW-DOGE','KRW-DOT','KRW-BCH','KRW-LINK','KRW-LTC','KRW-XLM']
-		
+		coinlist = ['BTC','ETH','ADA','XRP','DOGE','DOT','BCH','LINK','LTC','XLM']
+
 		url = 'https://api.upbit.com/v1/candles/days?market=KRW-BTC'
 		r = web.get(url)
 		r.raise_for_status()
@@ -65,7 +76,7 @@ def main(wf):
 		get_time(result)		
 
 		for coin in coinlist:
-			url = 'https://api.upbit.com/v1/candles/days?market={}'.format(coin)
+			url = 'https://api.upbit.com/v1/candles/days?market={}'.format('KRW-' + coin)
 			r = web.get(url)
 
 			r.raise_for_status()
@@ -78,7 +89,6 @@ def main(wf):
 				arg='https://upbit.com/exchange?code=CRIX.UPBIT.KRW-' + ticker,
 				valid=True,
 				icon='icon/{}.png'.format(ticker.lower()))
-			time.sleep(0.05) # 약간의 시간 텀을 줘야 API가 제대로 작동합니다.
 
 	wf.send_feedback()
 
